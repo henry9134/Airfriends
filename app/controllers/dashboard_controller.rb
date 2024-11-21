9c40450deb0139c15e8ccb7cf5b6143985f57396
Dylan Mayoral <dylanmayoral@gmail.com>
2024-11-21 13:49:31 +0900

    changes to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,9 +2,12 @@ class DashboardController < ApplicationController
   def index
     # @upcoming_bookings = Booking.where()
     @user = current_user
-    @upcoming_bookings = @user.rentals.where("booking_date >= ?", Date.today)
-    @confirmed_requests = @user.rentals.where(status: "accepted")
-    @hosted_rentals = Rental.joins(:activity).where(activities: {user_id: @user.id}).where("booking_date >= ?", Date.today).where(status: "accepted")
+
+    @accepted_rentals = @user.rentals.where(status: "accepted")
+    @pending_rentals = @user.rentals.where(status: "pending")
+    @declined_rentals = @user.rentals.where(status: "declined")
+
+    @confirmed_requests = Rental.joins(:activity).where(activities: {user_id: @user.id}).where("booking_date >= ?", Date.today).where(status: "accepted")
     @pending_requests = @user.requests.where(status: "pending")
   end
   # @upcoming_bookings.where(status: "pending").where.not(id: @user)
